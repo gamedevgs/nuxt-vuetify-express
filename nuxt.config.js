275d43236fdf45a6139e524f3e74b9e59a514dde
Quốc Hội <quochoi@tuoitre.com.vn>
2020-11-09 21:46:26 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,7 +98,7 @@ module.exports = {
      ** Build configuration
      */
     axios: {
-        baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+        baseUrl: process.env.BASE_URL
     },
     build: {
         transpile: ['vuetify/lib'],
